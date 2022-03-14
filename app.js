9e0c62e0393626fcffb157ea0fa8c76462fb24ab
chirag.ccdps28@gmail.com <chirag.ccdps28@gmail.com>
2022-03-14 21:59:29 +0530

    email verification

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-require('dotenv').config()
+// require('dotenv').config()
+const path = require('path');
 const express = require('express');
+const url = 'mongodb://localhost/UserDatabase';
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/api', (req,res) => {
-    res.json({
-        success: 1,
-        message: "this is rest api working"
-    })
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/api', (req,res) => {
+//     res.json({
+//         success: 1,
+//         message: "this is rest api working"
+//     })
+// })
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+const authRoutes = require('./routes/auth')
+app.use(authRoutes)
 
 //console.log("connected");
-app.listen(process.env.APP_PORT , () => {
-    console.log('server is listening on port ' + process.env.APP_PORT)
+app.listen(3000 , () => {
+    console.log('server is listening on port ' + 3000)
 }); 
+
+mongoose.connect(url)
+const con = mongoose.connection
+con.on('open', () => {
+    console.log("connected..");
+})
